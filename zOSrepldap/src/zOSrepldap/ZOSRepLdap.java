987545b0f615cf1664ac359c81fdc15fdd5318b3
Donald Faul <faudo01@ca.com>
2017-03-09 17:07:00 -0800

    US312116 Flesh in the DB query to use for Mainframe datasets

--- zOSrepldap/src/zOSrepldap/ZOSRepLdap.java
+++ zOSrepldap/src/zOSrepldap/ZOSRepLdap.java
@@ -15,7 +15,8 @@ public class ZOSRepLdap {
 		// Leaving empty		
 	}
 
-	private static String buildDsnCiaQuery(String stemMaster)
+	private static String buildDsnCiaQuery(String sProduct,
+			                               String stemMaster)
 	{
 		String[] stems = null;
 		boolean stemsExist = false;
@@ -37,8 +38,9 @@ public class ZOSRepLdap {
 		}
 				
 		String permQueryRoleTemplate1 =  "SELECT DISTINCT "
-											+ "P1.SYSID,"  
-										    + "P1.RESCLASS, " 
+			    							+ "P1.RESCLASS AS APP, " 
+											+ "P1.SYSID AS APP_INSTANCE,"
+			    							+ "\'"+sProduct+"\' AS PRODUCT,"
 			      					  		+ "SUBSTR(R1.USERID,1,8) AS USERID, "                             
 			      					  		+ "P1.AUTHTYPE, "                                                  
 			      					  		+ "SUBSTR(P1.AUTHID,1,8) AS ROLEID, "                             
@@ -55,8 +57,8 @@ public class ZOSRepLdap {
 			      					  		+ "P1.ACC_INQUIRE, "
 			      					  		+ "P1.ACC_SET, "
 			      					  		+ "P1.ADMINBY, "
-			      					  		+ "P1.ADMINDATE, "
-			      					  		+ "P1.ADMINTIME, "
+			      					  		//+ "P1.ADMINDATE, "
+			      					  		//+ "P1.ADMINTIME, "
 			      					  		+ "U1.NAME "
 			      					  + "FROM "                                                                 
 			      					  		+ "TABLE(CAIESQ.CIA_RESOURCE_MATCH('";
@@ -68,8 +70,9 @@ public class ZOSRepLdap {
 										+"AND   P1.AUTHID = R1.ROLEID ";
 		
 		String permQueryUserTemplate1 = "SELECT DISTINCT "
-											+ "P2.SYSID, "
-											+ "P2.RESCLASS, "
+											+ "P2.RESCLASS AS APP, "
+											+ "P2.SYSID AS APP_INSTANCE, "
+			    							+ "\'"+sProduct+"\' AS PRODUCT,"
 											+ "SUBSTR(P2.AUTHID,1,8) AS USERID, "                             
 											+ "P2.AUTHTYPE, "                                                 
 											+ "'   ' AS ROLEID, "                                              
@@ -86,8 +89,8 @@ public class ZOSRepLdap {
 											+ "P2.ACC_INQUIRE, "
 											+ "P2.ACC_SET, "
 											+ "P2.ADMINBY, "
-											+ "P2.ADMINDATE, "
-											+ "P2.ADMINTIME, "
+											//+ "P2.ADMINDATE, "
+											//+ "P2.ADMINTIME, "
 											+ "'   ' AS NAME "
 										+"FROM "       
 											+ "TABLE(CAIESQ.CIA_RESOURCE_MATCH('";
@@ -118,12 +121,14 @@ public class ZOSRepLdap {
 
 		return permQuery;
 	} // buildDsnCiaQuery
-	
+
+		
 	private static void readDBToRepoContainer(JCaContainer cRepoInfo, 
-            String sDB2Password) {
+            								  String sDB2Password,
+            								  String sQuery) {
 		PreparedStatement pstmt = null; 
 		String sqlStmt;
-		int iIndex = 0;
+		int iIndex = cRepoInfo.getKeyElementCount("APP");
 		ResultSet rSet;
 		
 		String sqlError = "DB2. Unable to execute query.";
@@ -133,12 +138,38 @@ public class ZOSRepLdap {
 			Class.forName("com.ibm.db2.jcc.DB2Driver");
 			Connection conn = DriverManager.getConnection(sJDBC);
 			
-			sqlError = "DB2. Error reading zOS Dataset records from CIA database.";
-			sqlStmt = frame.readTextResource("zOSDBQuery.txt", "IS NOT NULL", "", "", "");
+			sqlError = "DB2. Error reading z/OS Dataset records from CIA database.";
+			sqlStmt = sQuery;
 			pstmt=conn.prepareStatement(sqlStmt); 
 			rSet = pstmt.executeQuery();
 
 			while (rSet.next()) {
+				String sAuthType = rSet.getString("AUTHTYPE").trim();
+				String sRoleID = sAuthType.equalsIgnoreCase("R")? rSet.getString("ROLEID"): "";
+				
+				cRepoInfo.setString("APP",           rSet.getString("APP").trim(),                         iIndex);
+				cRepoInfo.setString("APP_INSTANCE",  rSet.getString("APP_INSTANCE").trim(),                iIndex);
+				cRepoInfo.setString("PRODUCT",       rSet.getString("PRODUCT").trim(),                     iIndex);
+				cRepoInfo.setString("AUTHTYPE",      sAuthType,                                            iIndex);
+				cRepoInfo.setString("ROLEID",        sRoleID.trim(),                                       iIndex);
+				cRepoInfo.setString("RESMASK",       rSet.getString("RESMASK").trim(),                     iIndex);
+				cRepoInfo.setString("CONTACT",       "",                                                   iIndex);
+				cRepoInfo.setString("ADMINISTRATOR", rSet.getString("ADMINISTRATOR").toLowerCase().trim(), iIndex);
+				cRepoInfo.setString("DEPARTMENT",    rSet.getString("RESOURCE_OWNER").toLowerCase().trim(),iIndex);
+				cRepoInfo.setString("USERID",        rSet.getString("USERID").toLowerCase().trim(),        iIndex);
+				//cRepoInfo.setString("USERNAME",      rSet.getString("FULLNAME").trim().replace(',', '|'),  iIndex);
+				cRepoInfo.setString("USERNAME",      rSet.getString("FULLNAME").trim(),                    iIndex);
+				cRepoInfo.setString("ACC_READ",      rSet.getString("acc_read").trim(),                    iIndex);
+				cRepoInfo.setString("ACC_WRITE",     rSet.getString("acc_write").trim(),                   iIndex);
+				cRepoInfo.setString("ACC_UPDATE",    rSet.getString("acc_update").trim(),                  iIndex);
+				cRepoInfo.setString("ACC_ALL",       rSet.getString("acc_all").trim(),                     iIndex);
+				cRepoInfo.setString("ACC_NONE",      rSet.getString("acc_none").trim(),                    iIndex);
+				cRepoInfo.setString("ACC_CREATE",    rSet.getString("acc_create").trim(),                  iIndex);
+				cRepoInfo.setString("ACC_FETCH",     rSet.getString("acc_fetch").trim(),                   iIndex);
+				cRepoInfo.setString("ACC_SCRATCH",   rSet.getString("acc_scratch").trim(),                 iIndex);
+				cRepoInfo.setString("ACC_CONTROL",   rSet.getString("acc_control").trim(),                 iIndex);
+				cRepoInfo.setString("ACC_INQUIRE",   rSet.getString("acc_inquire").trim(),                 iIndex);
+				cRepoInfo.setString("ACC_SET",       rSet.getString("acc_set").trim(),                     iIndex);			
 				iIndex++;
 			} // loop over record sets
 
@@ -178,8 +209,8 @@ public class ZOSRepLdap {
 			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 			Connection conn = DriverManager.getConnection(sJDBC);
 	
-			String sApp = "zOS Datasets";
-			String sApp2 = "MF20SCM";
+			String sApp = "Mainframe";
+			String sApp2 = "DATASET";
 
 			sqlError = "DB. Error deleting previous records.";
 			sqlStmt = "delete from GITHUB_REVIEW where Application in ('"+ sApp +"','"+ sApp2 + "')";
@@ -192,7 +223,54 @@ public class ZOSRepLdap {
 			int nRecordsWritten = 0;
 			int nBlock = 100;
 
-			// TBD
+			for (int iIndex=0,nRecords=0; iIndex<cRepoInfo.getKeyElementCount("APP"); iIndex++) {
+				if (!cRepoInfo.getString("APP", iIndex).isEmpty()) { 
+					if (nRecords%nBlock == 0)
+						sqlStmt = sqlStmt0;
+					else 
+						sqlStmt += " , ";
+					
+					sEntitlement2 = cRepoInfo.getString("AUTHTYPE", iIndex).equalsIgnoreCase("U")? "User" : "Role:"+cRepoInfo.getString("ROLEID", iIndex);
+					sContactEmail = "";
+					String[] aContacts = frame.readAssignedApprovers(cRepoInfo.getString("CONTACT", iIndex));
+					for (int j=0; j<aContacts.length; j++) {
+						if (!sContactEmail.isEmpty())
+							sContactEmail += ";";
+						if (aContacts[j].equalsIgnoreCase("toolsadmin"))
+							sContactEmail += "Toolsadmin@ca.com";
+						else
+							sContactEmail += aContacts[j]+"@ca.com";
+					}
+					sEntitlementAttrs = "adminby=" + cRepoInfo.getString("ADMINISTRATOR", iIndex);
+					sUserAttrs = "username=" +  cRepoInfo.getString("USERNAME", iIndex) + ";" +
+							     "read="     + (cRepoInfo.getString("ACC_READ", iIndex).equalsIgnoreCase("A")? "Y" : "N") + ";" +
+							     "write="    + (cRepoInfo.getString("ACC_WRITE", iIndex).equalsIgnoreCase("A")? "Y" : "N") + ";" +
+							     "update="   + (cRepoInfo.getString("ACC_UPDATE", iIndex).equalsIgnoreCase("A")? "Y" : "N") + ";" +
+							     "all="      + (cRepoInfo.getString("ACC_ALL", iIndex).equalsIgnoreCase("A")? "Y" : "N") + ";" +
+							     "none="     + (cRepoInfo.getString("ACC_READ", iIndex).equalsIgnoreCase("A")? "Y" : "N") ;
+					
+					sValues = "('"  + sApp + "',"+
+							  "'"   + cRepoInfo.getString("APP_INSTANCE", iIndex) + "',"+
+							  "'"   + cRepoInfo.getString("PRODUCT", iIndex) + "',"+
+							  "'"   + sEntitlement2 + "',"+
+							  "'"   + cRepoInfo.getString("RESMASK", iIndex) + "',"+
+							  "'"   + sEntitlementAttrs + "',"+
+							  "'"   + sContactEmail + "',"+
+							  "'"   + cRepoInfo.getString("USERID", iIndex) + "',"+
+							  "'"   + sUserAttrs + "')";
+					
+				    sqlStmt += sValues;
+				    
+				    if (nRecords%nBlock == (nBlock-1)) {
+						pstmt=conn.prepareStatement(sqlStmt);  
+						iResult = pstmt.executeUpdate();
+						if (iResult > 0) 
+							nRecordsWritten += iResult;	
+						sqlStmt = "";
+				    }
+					nRecords++;	
+				}
+			} // loop over records
 		
 		} catch (ClassNotFoundException e) {
 			iReturnCode = 301;
@@ -265,21 +343,27 @@ public class ZOSRepLdap {
 	        		sImagDBPassword = frame.AESDecrypt(environ.get(envName));
 	        }
 			// Write out processed records to database
-			//JCaContainer cRepoInfo = new JCaContainer();
-			//readDBToRepoContainer(cRepoInfo, sDB2Password);
+			JCaContainer cRepoInfo = new JCaContainer();
 
 			JCaContainer cContacts = new JCaContainer();
 			frame.readSourceMinderContacts(cContacts, "Mainframe");
-			// loop over contacts
+			// loop over contact records
+			for (int iIndex=0; iIndex<cContacts.getKeyElementCount("Location"); iIndex++) {
+				String sProduct    = cContacts.getString("Product", iIndex);
+				String sStemMaster = cContacts.getString("Location", iIndex);
+				String sQuery = buildDsnCiaQuery(sProduct, sStemMaster);
+				readDBToRepoContainer(cRepoInfo, sDB2Password, sQuery);
+			}
 			
+			// Set contact information
 			
 			// Write out processed repository in organization file
-			//if (!sOutputFile.isEmpty()) {
-			//	frame.writeCSVFileFromListGeneric(cRepoInfo, sOutputFile, '\t');					
-			//}
+			if (!sOutputFile.isEmpty()) {
+				frame.writeCSVFileFromListGeneric(cRepoInfo, sOutputFile, '\t');					
+			}
 			
 			// Write out processed records to database
-			//writeDBFromRepoContainer(cRepoInfo, sImagDBPassword);
+			writeDBFromRepoContainer(cRepoInfo, sImagDBPassword);
 			
 			if (!sProblems.isEmpty()) {
 				sProblems+="</ul>\n";
