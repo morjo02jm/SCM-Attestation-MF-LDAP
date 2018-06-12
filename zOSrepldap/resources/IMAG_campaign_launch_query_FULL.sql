af686c4065efea3d3fa643c80262ebbb2b6451d5
Donald Faul <faudo01@ca.com>
2018-06-12 15:44:05 -0700

    Changes to launch query where entitlement is product name only.

--- zOSrepldap/resources/IMAG_campaign_launch_query_FULL.sql
+++ zOSrepldap/resources/IMAG_campaign_launch_query_FULL.sql
@@ -1,7 +1,5 @@
-select
-     concat('Product: '+ github.[EntitlementOwner1],
-            '/ ' + github.[EntitlementOwner2] , 
-            '/ Resmask: ' + github.[EntitlementName]) as 'ResName1',
+select 
+     github.[EntitlementOwner1] as 'ResName1',
      '-' as 'ResName2',
      case 
         when len(github.[ContactEmail]) < 1 then 'Unknown' 
@@ -23,7 +21,7 @@ select
      'Active ('+emp.Status+')' as 'Status'
      FROM /* GITHUB_REVIEW as github */
      (     
-        select distinct R1.Application, R1.EntitlementOwner1, R1.EntitlementOwner2, R1.EntitlementName, R1.ContactEmail, R1.User_ID 
+        select distinct R1.Application, R1.EntitlementOwner1, R1.ContactEmail, R1.User_ID 
         from GITHUB_REVIEW R1
      ) github
      left join QAR_EMPINFO as emp on emp.SAM=github.[User_ID]
